51046afe379a958c6dd69e1d14f4da9c20fedf00
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2021-02-10 12:27:32 -0800

    add project files

---
+++ src/MiniPalette.js
@@ -0,0 +1,43 @@
+import React, {PureComponent} from 'react';
+import styles from './styles/MiniPaletteStyles';
+import { withStyles } from '@material-ui/styles';
+import DeleteIcon from "@material-ui/icons/Delete";
+
+class MiniPalette extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.deletePalette = this.deletePalette.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    deletePalette(e) {
+        e.stopPropagation();
+        this.props.openDialog(this.props.id);
+    }
+    handleClick() {
+        this.props.goToPalette(this.props.id);
+    }
+    render() {
+        const {classes, paletteName, emoji, colors } = this.props;
+        const miniColorBoxes = colors.map(color => (
+        <div 
+            className={classes.miniColor} 
+            key={color.name} 
+            style={{backgroundColor: color.color}}>
+        </div>
+    ));
+    return (
+        <div className={classes.root} onClick={this.handleClick}>
+            <DeleteIcon 
+                className={classes.deleteIcon} 
+                style={{transition: "all 0.3s ease-in-out"}}
+                onClick={this.deletePalette} />
+            <div className={classes.colors}>
+                {miniColorBoxes}
+            </div>
+            <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+        </div>
+    )
+    }
+}
+
+export default withStyles(styles)(MiniPalette);
---
+++ src/SingleColorPalette.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
+import styles from './styles/PaletteStyles';
+import { withStyles } from '@material-ui/styles';
+
+
+class SingleColorPalette extends Component {
+    constructor(props) {
+        super(props);
+        this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+        this.state = {
+            format: "hex"
+        };
+        this.changeFormat = this.changeFormat.bind(this);
+    }
+    gatherShades(palette, colorToFilterBy) {
+        // return all shades of given color
+        let shades = [];
+        let allColors = palette.colors;
+
+        for(let key in allColors) {
+            shades = shades.concat(
+                allColors[key].filter(color => color.id === colorToFilterBy)
+            );
+        }
+
+        return shades.slice(1);
+    }
+    changeFormat(val) {
+        this.setState({ format: val });
+      }
+  render() {
+      const {format} = this.state;
+      const {paletteName, emoji, id} = this.props.palette;
+      const {classes} = this.props;
+      const colorBoxes = this._shades.map(color => (
+          <ColorBox 
+            key={color.name} 
+            name={color.name} 
+            background={color[format]}
+            showingFullPalette={false} />
+      ));
+    return (
+      <div className={classes.Palette}>
+      <Navbar 
+        handleChange={this.changeFormat}
+        showingAllColors={false} />
+        <div className={classes.colors}>
+            {colorBoxes}
+            <div className={classes.goBack}>
+                <Link to={`/palette/${id}`}>go back</Link>
+            </div>
+        </div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(SingleColorPalette);
